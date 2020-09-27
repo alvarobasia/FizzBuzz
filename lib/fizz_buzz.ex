09f86a0f55ce718cff4fca749d63b4c234bf7bff
Álvaro Basílio de Araújo <alvarobasilioedmar@gmail.com>
2020-09-26 23:26:00 -0300

    refactor functions

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,26 +1,31 @@
 defmodule FizzBuzz do
   def build(file_name) do
     file_name
-    |>File.read()
-    |>headle_file()
-  end
-  def headle_file({:ok, content}) do
-    content
-    |>String.split(",")
-    |>Enum.map(&convert_and_evaluate/1)
-  end
-  def headle_file({:error, reason}) do
-    "Erro ao ler o arquivo. Erro: #{reason}"
+    |> File.read()
+    |> headle_file()
   end
 
-  def convert_and_evaluate(element) do
+  defp headle_file({:ok, content}) do
+    result =
+      content
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate/1)
+
+    {:ok, result}
+  end
+
+  defp headle_file({:error, reason}), do: {:error, "Erro ao ler o arquivo. Erro: #{reason}"}
+
+  defp convert_and_evaluate(element) do
     element
     |> String.to_integer()
     |> get_correct_numbers()
   end
 
-  def get_correct_numbers(number) when rem(number, 5) === 0 and rem(number, 3) === 0 , do: :fizzbuzz
-  def get_correct_numbers(number) when rem(number, 3) === 0, do: :fizz
-  def get_correct_numbers(number) when rem(number, 5) === 0, do: :buzz
-  def get_correct_numbers(number), do: number
+  defp get_correct_numbers(number) when rem(number, 5) === 0 and rem(number, 3) === 0,
+    do: :fizzbuzz
+
+  defp get_correct_numbers(number) when rem(number, 3) === 0, do: :fizz
+  defp get_correct_numbers(number) when rem(number, 5) === 0, do: :buzz
+  defp get_correct_numbers(number), do: number
 end
